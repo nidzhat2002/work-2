d2418f4a5148e643012c350d255e4bc33c6254ec
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-18 17:47:21 +0300

    Задание №8

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Clear();
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-if (a % 2 == 0) 
-     Console.WriteLine($"Число {a} является четным.");
-else    
-     Console.WriteLine($"Число {a} является нечетным.");
+Console.Write("Веедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int step = 1;
+if (n <= 0) step = -1;
+for (int i = 1; Math.Abs(i) <= Math.Abs(n); i += step)
+    if (i % 2 == 0)
+         Console.Write($"{i} ");
