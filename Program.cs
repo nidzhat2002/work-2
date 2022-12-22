cd373d9a5256eb86980cd606141d34f74e25da2f
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-23 01:43:12 +0300

    Задача №21

--- Program.cs
+++ Program.cs
@@ -1,23 +1,16 @@
-﻿bool NumberInInterval(int value, int min, int max)
-{
-if (value >= min && value <= max)
- return true;
-else 
-{
-Console.WriteLine($"Ошибка! Число должно находиться в интервале от {min} до {max}!");
- return false;
-}   
-}
-Console.Clear();
-string s;
-int n;
-do
-{
-Console.Write("Веедите пятизначное число: ");
-s = Console.ReadLine();
-n = Convert.ToInt32(s);
-} while (!NumberInInterval(n, 10000, 99999));
-if ((n / 10000 == n % 10) && (n % 10000 / 1000 == n % 100 / 10))
-    Console.WriteLine($"Число {n} - палиндром.");
-else
-    Console.WriteLine($"Число {n} - НЕ палиндром.");
+﻿Console.Clear();
+double x1, y1, z1, x2, y2, z2;
+Console.Write("Веедите координату X 1-й точки: ");
+x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Веедите координату Y 1-й точки: ");
+y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Веедите координату Z 1-й точки: ");
+z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Веедите координату X 2-й точки: ");
+x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Веедите координату Y 2-й точки: ");
+y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Веедите координату Z 2-й точки: ");
+z2 = Convert.ToDouble(Console.ReadLine());
+double d = Math.Round(Math.Sqrt((x2 - x1) * (x2 - x1) +(y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1)), 3);
+Console.WriteLine($"Расстояние между точками: {d}");
