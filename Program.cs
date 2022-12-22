93595a285a934f8888bb8bfe4979eef691c9d9ae
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-23 01:24:34 +0300

    Задача №15

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿Console.Clear();
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int m = n;
-if (n < 100)
-    Console.WriteLine($"Третьей цифры у числа {n} нет.");
+bool NumberInInterval(int value, int min, int max)
+{
+if (value >= min && value <= max)
+   return true;
 else 
 {
-    while (m > 999) m /= 10;
-    Console.WriteLine($"Третья цифра у числа {n}: {m % 10}.");
+Console.WriteLine($"Ошибка! Число должно находиться в интервале от {min} до {max}!");
+return false;
+}   
 }
+string[] DayOfWeek = new string[7] { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресение"};
+Console.Clear();
+int n;
+do
+{
+    Console.Write("Веедите номер дня недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+} while (!NumberInInterval(n, 1, 7));
+if ((n == 6) || (n == 7))
+    Console.Write($"{DayOfWeek[n - 1]} - выходной.");
+else
+    Console.Write($"{DayOfWeek[n - 1]} - рабочий день.");
