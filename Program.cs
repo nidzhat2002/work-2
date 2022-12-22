e4f2b9915c54dc9ba5e61554b68ef7a938412cd4
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-23 01:29:46 +0300

    Задача №10

--- Program.cs
+++ Program.cs
@@ -2,22 +2,19 @@
 bool NumberInInterval(int value, int min, int max)
 {
 if (value >= min && value <= max)
-   return true;
+    return true;
 else 
 {
 Console.WriteLine($"Ошибка! Число должно находиться в интервале от {min} до {max}!");
 return false;
 }   
 }
-string[] DayOfWeek = new string[7] { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресение"};
-Console.Clear();
 int n;
+Console.Clear();
 do
 {
-    Console.Write("Веедите номер дня недели: ");
-    n = Convert.ToInt32(Console.ReadLine());
-} while (!NumberInInterval(n, 1, 7));
-if ((n == 6) || (n == 7))
-    Console.Write($"{DayOfWeek[n - 1]} - выходной.");
-else
-    Console.Write($"{DayOfWeek[n - 1]} - рабочий день.");
+Console.Write("Веедите трехзначное число: ");
+n = Convert.ToInt32(Console.ReadLine());
+} while (!NumberInInterval(n, 100, 999));
+int m = n / 10 - (n / 100) * 10;
+Console.WriteLine($"Вторая цифра числа: {m}.");
