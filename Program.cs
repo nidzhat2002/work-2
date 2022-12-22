8186274b03a1e561c3a82735a3b0da964f1540b9
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-23 01:45:03 +0300

    Задача №23

--- Program.cs
+++ Program.cs
@@ -1,16 +1,5 @@
 ﻿Console.Clear();
-double x1, y1, z1, x2, y2, z2;
-Console.Write("Веедите координату X 1-й точки: ");
-x1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Веедите координату Y 1-й точки: ");
-y1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Веедите координату Z 1-й точки: ");
-z1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Веедите координату X 2-й точки: ");
-x2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Веедите координату Y 2-й точки: ");
-y2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Веедите координату Z 2-й точки: ");
-z2 = Convert.ToDouble(Console.ReadLine());
-double d = Math.Round(Math.Sqrt((x2 - x1) * (x2 - x1) +(y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1)), 3);
-Console.WriteLine($"Расстояние между точками: {d}");
+Console.Write("Веедите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+Console.WriteLine($"{i} -> {i * i * i}");
