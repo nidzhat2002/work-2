f43378709ea87dc04c891a83f815b211e858c994
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-24 03:57:41 +0300

    Задание №41

--- Program.cs
+++ Program.cs
@@ -1,19 +1,10 @@
-﻿void PrintArray(int[] arr)
+﻿Console.Clear();
+Console.Write("Введите элементы массива: ");
+double[] array = Console.ReadLine().Split(" ").Select(x => Convert.ToDouble(x)).ToArray();
+Console.WriteLine($"Массив: {string.Join(" ", array)}");
+int count = 0;
+foreach (double element in array)
 {
-Console.Write("[");
-for (int i = 0; i < arr.Length - 1; i++)
-    Console.Write($"{arr[i]}, ");
-Console.WriteLine($"{arr[arr.Length - 1]}]");
+if (element > 0.0) count++;
 }
-Console.Clear();
-Console.Write("Введите размер массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[n];
-for (int i = 0; i < n; i++)
-    array[i] = new Random().Next(0, 200);
-Console.Write("Исходный массив: ");
-PrintArray(array);
-int sum = 0;
-for (int i = 1; i < n; i+=2)
-    sum += array[i];
-Console.WriteLine($"Сумма элементов на нечетных позициях = {sum}.");
+Console.WriteLine($"Количество положительных элементов в массиве = {count}");
