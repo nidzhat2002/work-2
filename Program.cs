64db6bc194d7bb2344a05533397054cf644861c6
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-24 04:17:18 +0300

    Задание №47

--- Program.cs
+++ Program.cs
@@ -1,32 +1,32 @@
-﻿void InputMatrix(int[,] matrix)
+﻿double FloatRnd(double min, double max, int digits)
 {
-    for (int row = 0; row < matrix.GetLength(0); row++)
-        for (int col = 0; col < matrix.GetLength(1); col++)
-            matrix[row, col] = new Random().Next(-10, 11);
+    return Math.Round(new Random().NextDouble() * (max - min) + min, digits);
 }
-void OutputMatrix(int[,] matrix)
+void InputMatrix(double[,] matrix)
 {
     for (int row = 0; row < matrix.GetLength(0); row++)
     {
         for (int col = 0; col < matrix.GetLength(1); col++)
-            Console.Write($"{matrix[row, col]}\t");
+        {
+            matrix[row, col] = FloatRnd(-100.0, 100.0, 3);
+        }
+        Console.WriteLine();
+    }
+}
+void OutputMatrix(double[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            Console.Write($"{matrix[row, col]}   \t");
+        }
         Console.WriteLine();
     }
 }
 Console.Clear();
 Console.Write("Введите размеры матрицы: ");
 int[] size = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
-int[,] matrix = new int[size[0], size[1]];
+double[,] matrix = new double[size[0], size[1]];
 InputMatrix(matrix);
-Console.WriteLine("Исходная матрица:");
 OutputMatrix(matrix);
-Console.Write("Введите индексы матрицы: ");
-int[] pos = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
-if (pos[0] >= matrix.GetLength(0) || pos[1] >= matrix.GetLength(1))
-{
-    Console.WriteLine($"Элемента A[{pos[0]}, {pos[1]}] не существует.");
-}
-else 
-{
-    Console.WriteLine($"Элемент A[{pos[0]}, {pos[1]}] = {matrix[pos[0], pos[1]]}.");
-}
