c4af35e3ad9b713c18a07f26128f9d70b55ba447
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-24 03:42:05 +0300

    Задание №34

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿void PrintArray(int[] arr)
-{   
+{
 Console.Write("[");
 for (int i = 0; i < arr.Length - 1; i++)
     Console.Write($"{arr[i]}, ");
@@ -10,10 +10,11 @@ Console.Write("Введите размер массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 for (int i = 0; i < n; i++)
-{
- Console.Write($"Введите {i+1}-й элемент массива: ");
- array[i] = Convert.ToInt32(Console.ReadLine());
-}
-Console.WriteLine();
-Console.Write("Вы ввели массив: ");
+    array[i] = new Random().Next(100, 1000);
+Console.Write("Исходный массив: ");
 PrintArray(array);
+int count = 0;
+for (int i = 0; i < n; i++)
+if (array[i] % 2 == 0) 
+    count++;
+Console.WriteLine($"В массиве {count} четных элемент(а/ов).");
