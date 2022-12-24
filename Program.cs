6fbc42ea3f9b4abd4911b0da9c5c1265d409f0bb
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-24 04:13:24 +0300

    Задание №50

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
-﻿Console.Clear();
-Console.Write("Введите параметры (k, b) первой прямой: ");
-double[] line1 = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
-Console.Write("Введите параметры (k, b) второй прямой: ");
-double[] line2 =Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
-double x, y;
-if (Math.Abs(line1[0] - line2[0]) > 1e-6)
+﻿void InputMatrix(int[,] matrix)
 {
-    x = (line2[1] - line1[1]) / (line1[0] - line2[0]);
-    y = line1[0] * x + line1[1];
-    Console.WriteLine($"Точка перечения 2х прямых ({x}, {y}).");
+    for (int row = 0; row < matrix.GetLength(0); row++)
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            matrix[row, col] = new Random().Next(-10, 11);
+}
+void OutputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            Console.Write($"{matrix[row, col]}\t");
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Исходная матрица:");
+OutputMatrix(matrix);
+Console.Write("Введите индексы матрицы: ");
+int[] pos = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
+if (pos[0] >= matrix.GetLength(0) || pos[1] >= matrix.GetLength(1))
+{
+    Console.WriteLine($"Элемента A[{pos[0]}, {pos[1]}] не существует.");
 }
 else 
 {
-    Console.WriteLine("Прямые параллельны.");
+    Console.WriteLine($"Элемент A[{pos[0]}, {pos[1]}] = {matrix[pos[0], pos[1]]}.");
 }
