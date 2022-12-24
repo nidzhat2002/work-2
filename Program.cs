d28ba340c3f825447c2ad10fd478868292f8177f
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-24 04:22:28 +0300

    Задание №52

--- Program.cs
+++ Program.cs
@@ -1,32 +1,34 @@
-﻿double FloatRnd(double min, double max, int digits)
+﻿void InputMatrix(int[,] matrix)
 {
-    return Math.Round(new Random().NextDouble() * (max - min) + min, digits);
+    for (int row = 0; row < matrix.GetLength(0); row++)
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            matrix[row, col] = new Random().Next(-10, 11);
 }
-void InputMatrix(double[,] matrix)
+void OutputMatrix(int[,] matrix)
 {
     for (int row = 0; row < matrix.GetLength(0); row++)
     {
         for (int col = 0; col < matrix.GetLength(1); col++)
-        {
-            matrix[row, col] = FloatRnd(-100.0, 100.0, 3);
-        }
-        Console.WriteLine();
-    }
-}
-void OutputMatrix(double[,] matrix)
-{
-    for (int row = 0; row < matrix.GetLength(0); row++)
-    {
-        for (int col = 0; col < matrix.GetLength(1); col++)
-        {
-            Console.Write($"{matrix[row, col]}   \t");
-        }
+            Console.Write($"{matrix[row, col]}\t");
         Console.WriteLine();
     }
 }
 Console.Clear();
 Console.Write("Введите размеры матрицы: ");
 int[] size = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
-double[,] matrix = new double[size[0], size[1]];
+int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
+Console.WriteLine("Исходная матрица:");
 OutputMatrix(matrix);
+double[] means = new double[size[1]];
+for (int col = 0; col < matrix.GetLength(1); col++)
+{
+    means[col] = 0.0; // лишняя операция (на всякий случай)
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        means[col] += matrix[row, col];
+    }
+    means[col] /= (double)matrix.GetLength(0);
+}
+Console.WriteLine("Средние арифметические по столбцам: ");
+Console.WriteLine(string.Join("   ",means));
