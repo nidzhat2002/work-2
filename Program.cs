39b62d7225b3b9ea36247ed624ec4a4a28eabd55
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-25 22:44:46 +0300

    Задание №56

--- Program.cs
+++ Program.cs
@@ -15,31 +15,25 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
-int[,] ReleaseMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            for (int k = 0; k < matrix.GetLength(1) - j - 1; k++)
-            {
-                if (matrix[i, k] < matrix[i, k + 1])
-                {
-                    int tmp = matrix[i, k];
-                    matrix[i, k] =  matrix[i, k+1];
-                    matrix[i, k + 1] = tmp;
-                }
-            }
-        }
-    }
-    return matrix;
-}
 Console.Clear();
 Console.Write("Введите размер матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
-Console.WriteLine("Начальный массив:");
+Console.WriteLine("Исходная матрица:");
 PrintMatrix(matrix);
-Console.WriteLine("Конечный массив:");
-PrintMatrix(ReleaseMatrix(matrix));
+int minSum = 10 * size[1], sum, iMinSum = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum += matrix[i, j];
+    }
+    if (sum < minSum)
+    {
+        iMinSum = i;
+        minSum = sum;
+    }
+}
+Console.WriteLine($"Наименьшая сумма элементов содержится в строке {iMinSum + 1} и равна {minSum}.");
