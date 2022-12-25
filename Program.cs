ca43da0385f46ad000360e930ff35bd71dee3bf6
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-25 23:13:06 +0300

    Задание №66

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿string GetNumbers(int start, int end)
+﻿int GetSum(int start, int end)
 {
-    if (start == end) return end.ToString();
-    return (start < end) ? GetNumbers(start + 1, end) + $", {start}": 
-                            GetNumbers(start, end + 1) + $", {end}";
+    if (start == end) return end;
+    return (start < end) ? GetSum(start + 1, end) + start: 
+                            GetSum(start, end + 1) + end;
 }
 Console.Clear();
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Числа: {GetNumbers(m, n)}");
+Console.WriteLine($"Сумма чисел от {m} до {n} = {GetSum(m, n)}");
