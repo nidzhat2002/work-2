3f809c4607a62c8538570409c39666249903f8a5
nidzhat2002 <nicatt.agarzayev@hotmail.com>
2022-12-25 22:48:16 +0300

    Задание №58

--- Program.cs
+++ Program.cs
@@ -15,25 +15,41 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
-Console.Clear();
-Console.Write("Введите размер матрицы: ");
-int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-int[,] matrix = new int[size[0], size[1]];
-InputMatrix(matrix);
-Console.WriteLine("Исходная матрица:");
-PrintMatrix(matrix);
-int minSum = 10 * size[1], sum, iMinSum = 0;
-for (int i = 0; i < matrix.GetLength(0); i++)
+int[,] MultiMatrix(int[,] matrA, int[,] matrB)
 {
-    sum = 0;
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    int[,] result = new int[matrA.GetLength(0), matrB.GetLength(1)];
+    for (int i = 0; i < result.GetLength(0); i++)
     {
-        sum += matrix[i, j];
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            int tmp = 0;
+            for (int k = 0; k < matrA.GetLength(1); k++)
+            {
+                tmp += matrA[i,k] * matrB[k,j];
+            }
+            result[i,j] = tmp;
+        }
     }
-    if (sum < minSum)
-    {
-        iMinSum = i;
-        minSum = sum;
-    }
+    return result;
 }
-Console.WriteLine($"Наименьшая сумма элементов содержится в строке {iMinSum + 1} и равна {minSum}.");
+Console.Clear();
+Console.Write("Введите размер 1й матрицы: ");
+int[] sizeA = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrixA = new int[sizeA[0], sizeA[1]];
+Console.Write("Введите размер 2й матрицы: ");
+int[] sizeB = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrixB = new int[sizeB[0], sizeB[1]];
+if (sizeA[1] != sizeB[0])
+{
+    Console.WriteLine("ОШИБКА! Количество столбцов первой матрицы должно совпадать с количеством строк во второй!");
+    return;
+}
+InputMatrix(matrixA);
+InputMatrix(matrixB);
+Console.WriteLine("Исходная 1я матрица:");
+PrintMatrix(matrixA);
+Console.WriteLine("Исходная 2я матрица:");
+PrintMatrix(matrixB);
+int[,] matrixC = MultiMatrix(matrixA, matrixB);
+Console.WriteLine("Результат умножения:");
+PrintMatrix(matrixC);
